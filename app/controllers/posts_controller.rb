9be768ab4af9d497ba7c63c491400eb87133510a
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-03 22:50:15 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,10 +35,12 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+
   def post_data
     post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
+    render json: post.to_json(only: [:title, :description, :id], include: [ author: { only: [:name] }])
   end
+
 
   private
 
